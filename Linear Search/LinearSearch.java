c7788cadc45c7e7e9d4ece0ad5da7778c8c78391
lucifer <imvk009@gmail.com>
2023-05-12 23:08:59 +0530

    using for-each

--- Linear Search/LinearSearch.java
+++ Linear Search/LinearSearch.java
@@ -1,14 +1,22 @@
 class LinearSearch{
     public static void main(String[] args) {
         int [] arr = {23,112,4423,54,221,456,2321,89};
-        int target = 4423;
+        int target = 89;
         System.out.println(searchLinear(arr, target));
     }
 
     static String searchLinear(int [] arr, int target){
-        for (int i = 0; i <= arr.length; i++){
-            if(target == arr[i])
-                return "Element found at index = " + i;
+//        for (int i = 0; i <= arr.length; i++){
+//            if(target == arr[i])
+//                return "Element found at index = " + i;
+//        }
+
+//        Using for-each loop
+        int index = 0;
+        for(int i : arr){
+            if(target == i)
+                return "Element Found At Index " + index;
+            index++;
         }
         return "Element Not Found";
     }
