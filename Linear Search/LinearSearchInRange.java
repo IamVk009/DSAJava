991900375a44919a5f57e521b33139bdb0a2f8ca
lucifer <imvk009@gmail.com>
2023-05-13 17:45:38 +0530

    LinearSearchInRange

---
+++ Linear Search/LinearSearchInRange.java
@@ -0,0 +1,35 @@
+// Search for Element within particular range in an Array Using Linear Search Algorithm
+class LinearSearchInRange {
+    public static void main(String[] args) {
+        int arr[] = {12,23,34,45,56,67,78,89,90,100};
+        int target = 90;
+        int start = 0;
+        int end = 9;
+        System.out.println(searchInRange1(arr, target, start, end));
+    }
+
+//  Approach 1 : Using For Loop
+    static String searchInRange1(int arr[], int target, int start, int end){
+//      Edge Case Condition
+        if(arr.length == 0)
+            return "Invalid Array";
+
+        for (int i = start; i <= end; i++){
+            if(target == arr[i])
+                return "Element found at Index = " + i;
+        }
+        return "Element Not Found Within Given Range";
+    }
+
+//  Approach 2 : Using For-Each Loop
+    static String searchInRange2(int arr[], int target, int start, int end){
+        int index = 0;
+        for(int i : arr){
+            if(index >= start && index <= end && target == i){
+                return "Element Found At Index = " + index;
+            }
+            index++;
+        }
+        return "Element Not Found";
+    }
+}
