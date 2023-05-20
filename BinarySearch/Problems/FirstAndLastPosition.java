a84e00a92fd7d4593ee87bbf4c216b5f345591a8
lucifer <imvk009@gmail.com>
2023-05-20 23:54:25 +0530

    First and Last Position

---
+++ BinarySearch/Problems/FirstAndLastPosition.java
@@ -0,0 +1,13 @@
+// Leetcode 34 : Find First and Last Position of Element in Sorted Array -
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+class FirstAndLastPosition{
+    public static void main(String[] args) {
+        int nums[] = {5 ,7, 7, 8, 8, 10};
+        int target = 8;
+        int ans[] = getFirstAndLastPosition(nums, target);
+    }
+
+    static int[] getFirstAndLastPosition(int nums[], int target){
+        return new int[]{};
+    }
+}
