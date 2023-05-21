42ae478f1ccd74c9eed84940e69018fdd8c41a66
lucifer <imvk009@gmail.com>
2023-05-21 23:44:36 +0530

    Popsition In Infinite Sorted Array

---
+++ BinarySearch/Problems/PositionInInfiniteSortedArray.java
@@ -0,0 +1,11 @@
+class PositionInInfiniteSortedArray{
+    public static void main(String[] args) {
+        int nums[] = {11,22,33,44,55,66,77,88,99,100};
+        int target = 88;
+        System.out.println("Position In Array = " + getPosition(nums, target));
+    }
+
+    static int getPosition(int nums[], int target){
+        return 0;
+    }
+}
